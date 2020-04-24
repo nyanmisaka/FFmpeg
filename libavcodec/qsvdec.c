762b6f5630d2a95879bc3279075d42884ebbc5d1
Nyanmisaka <nst799610810@gmail.com>
2020-04-24 16:30:40 +0800

    Update qsvdec.c

--- libavcodec/qsvdec.c
+++ libavcodec/qsvdec.c
@@ -591,7 +591,7 @@ int ff_qsv_process_data(AVCodecContext *avctx, QSVContext *q,
     ret = qsv_decode_header(avctx, q, pkt, pix_fmt, &param);
 
     if (q->resize_expr && sscanf(q->resize_expr, "%dx%d",
-                                   &param->mfx.FrameInfo.CropW, &param->mfx.FrameInfo.CropH) != 2) {
+                                   param->mfx.FrameInfo.CropW, param->mfx.FrameInfo.CropH) != 2) {
         av_log(avctx, AV_LOG_ERROR, "Invalid resize expressions\n");
         ret = AVERROR(EINVAL);
         return ret;
